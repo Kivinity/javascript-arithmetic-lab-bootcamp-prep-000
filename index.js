307c0ae3e00699f29e534fa454f6665bb069dd00
Kathryn Stenberg <kate.stenberg@hotmail.com>
2019-07-15 21:11:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ var number = 10
   return (n+1)
 }
 
+function dec(n)
+{
+  return (n-1)
+}
+
+function makeInt(n)
+{
+  return parseInt(n,10)
+}
+
 function preserveDecimal(n)
 {
   return parseFloat(n)
